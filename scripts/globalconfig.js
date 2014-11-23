754df27be0a868c07027e3ce6bb7cfa45cf96ad0
Andy Milenius <andrew.milenius@gmail.com>
2014-11-22 17:13:00 -0800

    asd

--- scripts/globalconfig.js
+++ scripts/globalconfig.js
@@ -1,6 +1,20 @@
 
 $(document).ready(function (){
 
-	console.log("website config");
+	/*
+	<head>
+	 */
+
+	$("title").text("ecstatichabit");
+
 	$("#website-title").text("Cafe Sandy");
+
+	var leftNavLinks = [
+		{ href: "http://mileni.us/me", text: "Me"},
+		{ href: "http://mileni.us/toolbox", text: "Toolbox"}
+	];
+
+	for (var i = 0; i < leftNavLinks.length; i++) {
+		$("#left-nav-links").append('<a href="'+leftNavLinks[i].href+'">'+leftNavLinks[i].text+'</a>');
+	};
 });
